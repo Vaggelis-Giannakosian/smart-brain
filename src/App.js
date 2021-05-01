5fa0f39a09b002241f102760a93ce34dc0311f72
Vangelis Giannakosian <vangelis@telesto.gr>
2021-05-01 12:05:43 +0300

    the app should be able to detect multiple faces

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const particlesOptions = {
 const initialState = {
   input: '',
   imageUrl: '',
-  box: {},
+  box: [],
   route: 'signin',
   isSignedIn: false,
   user: {
@@ -54,16 +54,20 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
+
+    return data.outputs[0].data.regions.map(region=>{
+      const clarifaiFace = region.region_info.bounding_box;
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+    })
+
   }
 
   displayFaceBox = (box) => {
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,18 @@ const FaceRecognition = ({ imageUrl, box }) => {
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+          <>
+              {
+                  box.map((b,index)=> <div key={index}
+                                           className='bounding-box'
+                                           style={{
+                                               top: b.topRow,
+                                               right: b.rightCol,
+                                               bottom: b.bottomRow,
+                                               left: b.leftCol
+                                           }}/>)
+              }
+          </>
       </div>
     </div>
   );
