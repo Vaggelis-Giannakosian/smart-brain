45fd619c55a3747f49513bb2788b229931fe146d
Vangelis Giannakosian <vangelis@telesto.gr>
2021-05-02 17:24:39 +0300

    added reactstrap created ProfileIcon.js added ProfileIcon to Navigation

---
+++ src/components/Profile/ProfileIcon.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import {
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+import './ProfileIcon.css';
+
+const ProfileIcon = ({ onRouteChange }) => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+
+
+    return (
+        <div className="pa4 tc">
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle
+                    tag="span"
+                    data-toggle="dropdown"
+                    aria-expanded={dropdownOpen}
+                >
+                    <img src="http://tachyons.io/img/logo.jpg" alt="avatar"
+                         className="br-100 ba h3 w3 dib"/>
+                </DropdownToggle>
+                <DropdownMenu className="b--transparent shadow-5"
+                              style={{marginTop:'20px', backgroundColor:'rgba(255,255,255,.5)'}}
+                >
+                    <DropdownItem>View Profile</DropdownItem>
+                    <DropdownItem onClick={() => onRouteChange('signout')}>Sign Out</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        </div>
+    );
+}
+
+
+export default ProfileIcon;
